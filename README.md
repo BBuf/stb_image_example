7df57801b52c66e4f814bc32e49cadd57d27398b
BBuf <1182563586@qq.com>
2022-10-12 17:27:37 +0800

    refine

--- README.md
+++ README.md
@@ -4,15 +4,24 @@
 
 ```shell
 mkdir build
-cmake ..
+cmake -DENABLE_OPENCV=ON ..
 make -j4
 ```
+
+- 其中 -DENABLE_OPENCV=ON 表示启用OpenCV库，等于OFF表示关闭OpenCV库。
 
 请调整一下main.cpp的图片的路径，然后会输出基于stb做encoder，decoder以及resize的目标图片（尺度为原始图片的1/2）
 
 ## Performance
 
-后续提供和opencv对比的性能测试结果
+image_read+image_resize+image_write pipline.
+
+|库|循环迭代次数|耗时|
+| -- | -- | -- |
+| opencv | 10 | 0.338s |
+| stb    | 10 | 2.2776s|
+
+- OpenCV 版本：4.6.0
 
 ## Result
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #define STB_IMAGE_IMPLEMENTATION
 #include "include/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -5,34 +6,54 @@
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "include/stb_image_resize.h"
 #include <iostream>
+#include <time.h>
+#include <opencv2/opencv.hpp>
 using namespace std;
+using namespace cv;
 
-int main() {
-	std::cout << "Hello, STB_Image" << std::endl;
+int main(int argc,char *argv[]) {
+	assert(argc == 2);
+	clock_t st, en;
+	st = clock();
+	for(int i = 0; i < 10; i++){
+		string inputPath = "/home/zhangxiaoyu/stb_image_example/image/zidane.jpg";
+		if(string(argv[1]) == "stb"){
+			int iw, ih, n;
+		
+			// 加载图片获取宽、高、颜色通道信息
+			unsigned char *idata = stbi_load(inputPath.c_str(), &iw, &ih, &n, 0);
 
-	string inputPath = "/home/zhangxiaoyu/stb_image_encoder_decoder/image/zidane.jpg";
-	int iw, ih, n;
+			std::cout << iw << ", " << ih << ", " << n << std::endl;
+			int ow = iw / 2;
+			int oh = ih / 2;
+			auto *odata = (unsigned char *)malloc(ow * oh * n);
 
-	// 加载图片获取宽、高、颜色通道信息
-	unsigned char *idata = stbi_load(inputPath.c_str(), &iw, &ih, &n, 0);
+			// 改变图片尺寸
+			stbir_resize(idata, iw, ih, 0, odata, ow, oh, 0, STBIR_TYPE_UINT8, n, STBIR_ALPHA_CHANNEL_NONE, 0,
+				STBIR_EDGE_CLAMP, STBIR_EDGE_CLAMP,
+				STBIR_FILTER_BOX, STBIR_FILTER_BOX,
+				STBIR_COLORSPACE_SRGB, nullptr
+				);
 
-    std::cout << iw << ", " << ih << ", " << n << std::endl;
-	int ow = iw / 2;
-	int oh = ih / 2;
-	auto *odata = (unsigned char *)malloc(ow * oh * n);
+			string outputPath = "./resize_out.jpg";
+			// 写入图片
+			stbi_write_png(outputPath.c_str(), ow, oh, n, odata, 0);
 
-	// 改变图片尺寸
-	stbir_resize(idata, iw, ih, 0, odata, ow, oh, 0, STBIR_TYPE_UINT8, n, STBIR_ALPHA_CHANNEL_NONE, 0,
-		STBIR_EDGE_CLAMP, STBIR_EDGE_CLAMP,
-		STBIR_FILTER_BOX, STBIR_FILTER_BOX,
-		STBIR_COLORSPACE_SRGB, nullptr
-		);
-
-	string outputPath = "./resize_out.jpg";
-	// 写入图片
-	stbi_write_png(outputPath.c_str(), ow, oh, n, odata, 0);
-
-	stbi_image_free(idata);
-	stbi_image_free(odata);
+			stbi_image_free(idata);
+			stbi_image_free(odata);
+		}
+		else if(string(argv[1]) == "opencv"){
+			cv::Mat img = cv::imread(inputPath);
+			cv::Mat result;
+			cv::resize(img, result, Size(img.cols/2, img.rows/2));
+			string outputPath = "./resize_out.jpg";
+			cv::imwrite(outputPath, result);
+		}
+		else{
+			std::cout << "not support yet, exit successful!";
+		}
+	}
+	en = clock();
+	std::cout << "time = "<< double(en-st) / CLOCKS_PER_SEC << "s" <<endl;
 	return 0;
 }
